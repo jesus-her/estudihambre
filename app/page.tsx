c6889cec2606ec03a19c52a5889f6137df87b0c4
Jesus <jesus10.hn@gmail.com>
2023-11-15 15:20:57 -0600

    v2.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"force-dynamic";
 import Container from "@/app/components/Container";
 import ListingCard from "@/app/components/listings/ListingCard";
 import EmptyState from "@/app/components/EmptyState";
